6a1721c8df0fe8d7bef58857aa1f452f46221de7
Azeezah <azeezahm14@gmail.com>
2018-09-25 18:05:19 -0400

    Show correlations

--- show.py
+++ show.py
@@ -1,4 +1,7 @@
 import matplotlib.pyplot as plt
+from math import sqrt
+from random import choice
+
 the_important_columns = ["Time", "Occupant Number", "INDOOR Ambient Temp.", "INDOOR Relative Humidity", "INDOOR Air Velocity", "INDOOR Mean Radiant Temp.", "Predicted Mean Vote (PMV)"]
 
 def show_all(data, cols=the_important_columns):
@@ -17,6 +20,12 @@ def show(data, col_num=0, filename="graph.png"):
   #plt.axis([0, 60, 0, 1])
   plt.savefig(filename)
 
+def show_correlations(data):
+  cols = [get_col(label, data) for label in the_important_columns]
+  for i, x in enumerate(the_important_columns):
+    for j, y in enumerate(the_important_columns):
+      print x, 'to', y, correlation(numeric(cols[i]), numeric(cols[j]))
+
 def get_col(col_name, data):
   return [row[col_name] for row in data]
 
@@ -30,3 +39,21 @@ def numeric(xs):
 
 def stats(x):
   return ('len', len(x), 'avg', sum(x)/len(x))
+
+def stddev(xs):
+  mu = sum(xs)/len(xs)
+  return sqrt(sum((x-mu)**2 for x in xs) / (len(xs)-1))
+
+def covariance(xs, ys):
+  x_mu = sum(xs)/len(xs)
+  y_mu = sum(ys)/len(ys)
+  n = min(len(xs), len(ys))-1
+  return sum((x_mu-x) * (y_mu-y) for x, y in zip(xs, ys)) / n
+
+def correlation(xs, ys):
+  try:
+    return covariance(xs, ys)/(stddev(xs)*stddev(ys))
+  except ZeroDivisionError:
+    return 1
+
+
